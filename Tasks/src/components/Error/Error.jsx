740781e3c61f91f8de5979e4fe22f864f3554120
Hr1t1k <hritikkaushik@gmail.com>
2023-11-06 01:57:26 +0530

    updated error page

--- Tasks/src/components/Error/Error.jsx
+++ Tasks/src/components/Error/Error.jsx
@@ -1,6 +1,12 @@
-
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Error from "./Error.module.css"
 export default ()=> {
+  const navigate=useNavigate();
   return (
-    <h1>Error</h1>
+    <div className="d-flex flex-column flex-wrap align-items-center justify-content-center text-center" style={{height:"80vh"}}>
+        <h1 >Something unexpected happened,</h1>
+        <button className={`${Error.button} `} onClick={()=>navigate("/")}>Go back to homepage.</button>
+    </div>
   );
 }
